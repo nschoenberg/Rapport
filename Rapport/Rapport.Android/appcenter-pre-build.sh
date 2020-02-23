8193299b81dd815ecbbe562f9ba12861e2122049
Nicolai Schönberg <nicolai.schoenberg@gmail.com>
2020-02-23 16:52:41 +0100

    try fix android #2

--- Rapport/Rapport.Android/appcenter-pre-build.sh
+++ Rapport/Rapport.Android/appcenter-pre-build.sh
@@ -11,6 +11,6 @@ fi
 SECRETS_FILE="$APPCENTER_SOURCE_DIRECTORY/Rapport/Rapport/Resources/secrets.txt"
 
 echo $SECRETS_FILE
-echo $SECRETS_CONTENT > $SECRETS_FILE
+echo $SECRETS_CONTENT > SECRETS_FILE
 echo "File content:"
 cat SECRETS_FILE
