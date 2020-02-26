fc31991ee7fbd2b224c0db2f5d6c3f77ce5507cf
Nicolai Schönberg <nicolai.schoenberg@gmail.com>
2020-02-26 21:12:36 +0100

    Added Android App Icon

---
+++ Rapport/Rapport/Converter/CollectionNotEmptyToBooleanConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Rapport.Converter
+{
+    public class CollectionNotEmptyToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IList collection)
+            {
+                return collection.Count != 0;
+
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
